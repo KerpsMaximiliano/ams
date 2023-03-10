9b5a2144c4805a101af1dc224caa62f2d115f061
Lucio Cesar Cocuccio <lucio.cocuccio@i2t-sa.com.ar>
2023-03-10 11:24:54 -0300

    fixes after api refactor

--- src/app/core/models/tipo-nacionalidad.ts
+++ src/app/core/models/tipo-nacionalidad.ts
@@ -1,7 +1,6 @@
 export interface TipoNacionalidad {
     par_modo: string,
-    id_tabla: 3,// -> SIEMPRE ES 3
     codigo_nacionalidad_nuevo: number,
     descripcion: string,
-    codigo_nacionalidad: number
+    codigo_sistema_anterior: number
 }
